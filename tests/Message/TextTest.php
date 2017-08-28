a6cf60fec53bc6b19e50178fb235bc6bf3e22d51
Emanuel Limeira <emanuellimeira@gmail.com>
2017-08-28 00:43:22 -0300

    first commit

---
+++ tests/Message/TextTest.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace CodeBot\Message;
+
+use  PHPUnit\Framework\TestCase;
+
+class TextTest extends TesteCase
+{
+	
+	public function testRetornaUmArray ()
+	{
+		$actual = (new Text)->message('Oiii');
+		$expected = [
+		'recipient' => [
+			'id'=>1
+		],
+		'message' => [
+			'text' => 'Oiii',
+			'metadata' => 'DEVELOPER_DEFINED_METADATA'
+		]
+		];
+
+		$this->assertEquals($actual, $expected); 
+	}
+}
